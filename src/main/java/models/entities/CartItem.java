5370a360b1cb1d2747580f84e57fd871ca3ee1f7
NMS1010 <nguyenminhson102002@gmail.com>
2022-11-24 21:47:12 +0700

    add entities for mapping

---
+++ src/main/java/models/entities/CartItem.java
@@ -0,0 +1,77 @@
+package models.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cart_items",uniqueConstraints =
+@UniqueConstraint(columnNames = {"productId","cartId"}))
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartItemId;
+
+    @Column(nullable = false)
+    private int productId;
+
+    @Column(nullable = false)
+    private int cartId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime dateAdded;
+
+    @Column(nullable = false)
+    private int status;
+
+    public int getCartItemId() {
+        return cartItemId;
+    }
+
+    public void setCartItemId(int cartItemId) {
+        this.cartItemId = cartItemId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDateTime getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDateTime dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/models/entities/OrderItem.java
@@ -0,0 +1,76 @@
+package models.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items",uniqueConstraints =
+@UniqueConstraint(columnNames = {"productId","orderId"}))
+public class OrderItem {
+    @Id
+    @Column(name = "orderItemId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderItemId;
+
+    @Column(nullable = false)
+    private int orderId;
+
+    @Column(nullable = false)
+    private int productId;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+    @Column(nullable = false)
+    private int quantity;
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(int orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
