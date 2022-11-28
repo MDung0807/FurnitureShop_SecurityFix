810ce9df566ec90948452bd1477aeea42550b5a5
MinhDung08 <dominhdung21082002@gmail.com>
2022-11-28 20:49:27 +0700

    remove product

--- src/main/java/controllers/admin/product/RemoveProduct.java
+++ src/main/java/controllers/admin/product/RemoveProduct.java
@@ -1,2 +1,29 @@
-package controllers.admin.product;public class RemoveProduct {
+package controllers.admin.product;
+
+import models.services.product.ProductService;
+import utils.ServletUtils;
+import utils.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RemoveProduct", value = "/admin/product/delete")
+public class RemoveProduct extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int productId = StringUtils.toInt(request.getParameter("productId"));
+        boolean isSuccess = ProductService.getInstance().deleteProduct(productId);
+        String error = "";
+        if(!isSuccess){
+            error = "?error = true";
+        }
+        ServletUtils.redirect(response,request.getContextPath() + "/admin/products" + error);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
