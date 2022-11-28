d0994711116829d4eaf73130a61c47cd51da02b5
NMS1010 <nguyenminhson102002@gmail.com>
2022-11-29 01:03:45 +0700

    add role service + repos

---
+++ src/main/java/models/services/role/IRoleService.java
@@ -0,0 +1,17 @@
+package models.services.role;
+
+import models.view_models.roles.RoleCreateRequest;
+import models.view_models.roles.RoleGetPagingRequest;
+import models.view_models.roles.RoleUpdateRequest;
+import models.view_models.roles.RoleViewModel;
+
+import java.util.ArrayList;
+
+public interface IRoleService {
+    int insertRole(RoleCreateRequest request);
+    boolean updateRole(RoleUpdateRequest request);
+    boolean deleteRole(Integer roleId);
+    RoleViewModel retrieveRoleById(Integer roleId);
+    ArrayList<RoleViewModel> retrieveAllRole(RoleGetPagingRequest request);
+
+}
